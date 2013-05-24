cf1da5064acbb87c8374f2b3131bf389ae3bd40b
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-05-24 09:35:39 +0800

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ActiveRecord::Base
-	has_attached_file :avatar,
-	:styles => { :original => '250x250>',   
-                               :small => '50x50' }
-	has_many :reverts
-	belongs_to :node
-  attr_accessible :content, :node_id, :title, :user_id, :avatar
+    has_attached_file :avatar,:styles => { :original => '250x250>',:small => '50x50' }
+    has_many :reverts
+    belongs_to :node
+    attr_accessible :content, :node_id, :title, :user_id, :avatar
+    validates_presence_of :content, :node_id, :title, :user_id
 end
