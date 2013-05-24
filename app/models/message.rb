1a40746c52bf0f8d5ecbb30a7832d4fd8b2f44bb
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-05-24 09:33:43 +0800

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
   attr_accessible :content, :receive_id, :title, :user_id
+  validates_presence_of :content, :receive_id, :title, :user_id
 end
