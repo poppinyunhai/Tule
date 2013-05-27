9ed58f545ee6efe6a98f66a48ca5b71c48fe1c59
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-05-28 07:48:14 +0800

    Update reverts_controller.rb

--- app/controllers/reverts_controller.rb
+++ app/controllers/reverts_controller.rb
@@ -1,16 +1,17 @@
 class RevertsController < ApplicationController
-	before_filter :find_post
-	def new
-		@revert = Revert.new
-	end
+  before_filter :find_post
+  
+  def new
+    @revert = Revert.new
+  end
 
-	def create
-		@revert = Revert.create(:revertc => params[:revert]
-			[:revertc], :user_id => current_user.id)		
-	end
+  def create
+    @revert = Revert.create(:revertc => params[:revert]
+      [:revertc], :user_id => current_user.id)		
+  end
 
-	protected
-	def find_post
-  		@post = post.find( params[:post_id] )
-	end
+  protected
+  def find_post
+    @post = post.find( params[:post_id] )
+  end
 end
