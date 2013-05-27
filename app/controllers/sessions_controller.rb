2a6294f05d2c2cef8576e755168f9f6130a98396
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-05-28 07:50:04 +0800

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
-	def new
+  def new
 		
-	end
+  end
 
-	def create
-    	user = User.find_by_email(params[:session][:email].downcase)
-    	if user && user.authenticate(params[:session][:password])
-      		sign_in user
-      		redirect_to user
-    	else
-      		flash.now[:error] = 'Invalid email/password combination'
-      		render 'new'
-    	end
-  	end
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
-	def destroy
+  def destroy
     sign_out
     redirect_to root_path
   end
