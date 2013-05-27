88c4e09bc2089c723b2b9ed13aee684d92d1ccac
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-05-28 07:39:58 +0800

    Update node_posts_controller.rb

--- app/controllers/node_posts_controller.rb
+++ app/controllers/node_posts_controller.rb
@@ -1,15 +1,13 @@
 class NodePostsController < ApplicationController
   before_filter :find_node
-	def index
-		@post = Post.where(:node_id => @node.id)	
-		
-	end
+    def index
+      @post = Post.where(:node_id => @node.id)		
+    end
 
 
 
-	protected
-	def find_node
-		@node = Node.find(params[:node_id])
-		
-	end
+  protected
+  def find_node
+    @node = Node.find(params[:node_id])		
+  end
 end
