580cc8b7008f5d05dab4b88252511f45fc5a63af
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-05-28 08:02:26 +0800

    Update revert.rb

--- app/models/revert.rb
+++ app/models/revert.rb
@@ -1,7 +1,7 @@
 class Revert < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :post
-    attr_accessible :post_id, :revertc, :user_id
-    validates_presence_of :post_id, :revertc, :user_id
-    validates_length_of :revertc, :maximum => 500
+  belongs_to :user
+  belongs_to :post
+  attr_accessible :post_id, :revertc, :user_id
+  validates_presence_of :post_id, :revertc, :user_id
+  validates_length_of :revertc, :maximum => 500
 end
